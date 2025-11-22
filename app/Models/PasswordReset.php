a13cead20b4b56a6df4ef080bfc6da018659eacb
Hensuri <hensleytantrawan005@gmail.com>
2025-11-22 23:09:07 +0700

    Prevent Brute Force For Password Reset
    Adding logic to send new verification code, if entering the wrong code

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PasswordReset extends Model
 {
     protected $table = 'password_reset';
-    protected $fillable = ['email', 'code_hash', 'reset_token', 'expires_at', 'used'];
+    protected $fillable = ['email', 'code_hash', 'reset_token', 'expires_at', 'used', 'attempt'];
 
     protected $casts = [
         'expires_at' => 'datetime',
